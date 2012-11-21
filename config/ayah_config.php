b227ef7cf8cc5771aab4ab21696dae30f2afe6a5
Joe Auty <joe@netmusician.org>
2012-11-21 18:59:06 -0500

    initial commit

---
+++ config/ayah_config.php
@@ -0,0 +1,15 @@
+<?php
+// Edit the two lines below to use the keys for your site.
+// (Note: you can find your keys at http://portal.areyouahuman.com/dashboard)
+//define( 'AYAH_PUBLISHER_KEY', 'your_publisher_key_goes_here');
+//define( 'AYAH_SCORING_KEY', 'your_scoring_key_goes_here');
+
+define( 'AYAH_PUBLISHER_KEY', 'fa695fa0040c016f352b8e9434a57d21ef86cc48');
+define( 'AYAH_SCORING_KEY', 'eeb7e17af80d1b7c28899abd8c93596c73052a2b');
+
+
+// Point to the default AYAH web service and set the timeout and debug_mode.
+// (Note: you do not need to change these.)
+define( 'AYAH_WEB_SERVICE_HOST', 'ws.areyouahuman.com');
+define( 'AYAH_TIMEOUT', 0);
+define( 'AYAH_DEBUG_MODE', FALSE);
